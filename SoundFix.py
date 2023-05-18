da4638b1d9af64822067df15ea4f5122879c85a4
Gisela Leon Pipo <gisela.leon@estudiantat.upc.edu>
2023-05-18 18:55:50 +0200

    Act

--- SoundFix.py
+++ SoundFix.py
@@ -3,16 +3,18 @@ import soundfile as sf
 import sounddevice as sd
 from pydub import AudioSegment
 
-# Funció qie llegeix un fitxer d'àudio en .mp3 i el transforma a .wav
-def convert2wav(fitxer_mp3, fitxer_wav):
+# Funció que llegeix un fitxer d'àudio en .mp3 i el transforma a .wav
+fm = 10000
+fitxer_mp3, fm = sf.read("Summer_Wine.mp3")
+
+def convert2wav(fitxer_mp3):
     """
     Agafa com a arguments fitxer_mp3, que es la ruta del fitxer mp3 que es
     desitja convertir i fitxer_wav, que es la ruta del fitxer wav de sortida.
     """
     audio = AudioSegment.from_mp3(fitxer_mp3)
     audio.export(fitxer_wav, format = "wav") # Es guarda l'àudio en format WAV especificant la ruta i el format desitjat
+    return fitxer_wav
 
-fitxer_mp3 = "Summer_Wine.mp3"
-fitxer_wav = "Algo_bonito.wav"
-
-convert2wav(fitxer_mp3, fitxer_wav)    
+fitxer_wav = convert2wav(fitxer_mp3)
+sf.write("Algo_bonito.wav", fitxer_wav, fm)   
